5e704679a7885dec878c17b73dbbe43bcac80a30
Samantha Oliver <soliver@usgs.gov>
2020-07-07 21:55:28 -0500

    turned data file into object, and object into data file. Did not have two objects to turn into files!

--- 3_visualize/src/plot_timeseries.R
+++ 3_visualize/src/plot_timeseries.R
@@ -1,6 +1,6 @@
-plot_nwis_timeseries <- function(fileout, site_data_styled, width = 12, height = 7, units = 'in'){
-  
-  ggplot(data = site_data_styled, aes(x = dateTime, y = water_temperature, color = station_name)) +
+plot_nwis_timeseries <- function(fileout, site_dat, width = 12, height = 7, units = 'in'){
+  site_data = read_csv(site_dat)
+  ggplot(data = site_data, aes(x = dateTime, y = water_temperature, color = station_name)) +
     geom_line() + theme_bw()
   ggsave(fileout, width = width, height = height, units = units)
 }
